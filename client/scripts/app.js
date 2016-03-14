d87b5163ff4a0b545dfe9dfcdc6104875d275c45
somethiiing <wilsonweichengyu@gmail.com>
2016-03-14 14:49:49 -0700

    Make app.js send a POST request via ajax

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,2 +1,14 @@
-// YOUR CODE HERE:
+var App = function (){
 
+};
+
+App.prototype.init = function () {
+
+};
+
+App.prototype.send = function (message) {
+  $.ajax('http://www.willfulbard.com', {type: 'POST'});
+
+};
+
+var app = new App();
