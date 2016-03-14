15a80eb78e6bfe86b91e92b36683b61b424fbc60
somethiiing <wilsonweichengyu@gmail.com>
2016-03-14 15:00:17 -0700

    Make all sending tests pass

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -7,8 +7,28 @@ App.prototype.init = function () {
 };
 
 App.prototype.send = function (message) {
-  $.ajax('http://www.willfulbard.com', {type: 'POST'});
-
+  //message should already be in the following format:
+  /*
+  var message = {
+    username: 'shawndrost',
+    text: 'trololo',
+    roomname: '4chan'
+  };
+  */
+  $.ajax({
+    // This is the url you should use to communicate with the parse API server.
+    url: 'https://api.parse.com/1/classes/messages',
+    type: 'POST',
+    data: JSON.stringify(message),
+    contentType: 'application/json',
+    success: function (data) {
+      console.log('chatterbox: Message sent');
+    },
+    error: function (data) {
+      // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+      console.error('chatterbox: Failed to send message', data);
+    }
+  });
 };
 
 var app = new App();
